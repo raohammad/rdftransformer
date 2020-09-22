4c26e836fdc019e593f49fe27b5f817aefacadfe
Hammad Aslam Khan <raohammad@gmail.com>
2020-09-22 17:57:54 +0200

    modified home and about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    
+    <h1>For information, contact raohammad.at.gmail.com</h1>
   </div>
-    
 </template>
-
 <script>
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="fukhtakhin"/>
+    <HelloWorld msg="Semantic Data Transformer"/>
   </div>
 </template>
 
